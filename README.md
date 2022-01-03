e49e08fada0285d9f90cf8d4d7b402e7a15fd127
CamClrt <camille.clarret@gmail.com>
2022-01-03 08:48:45 +0100

    Coverage, improve run & report results

---
+++ README.md
@@ -0,0 +1,13 @@
+## Personal RSS feed manager, my alternative to Feedly (in local, at least)
+
+Inspired by [RealPython Tutorial](https://realpython.com/build-a-content-aggregator-python) 💙
+
+#### Mini roadmap / brainstorming
+
+* determine physical data model & models
+* unit and integration tests on models & more
+* setup a personal cmd to upload data
+* develop the admin backend
+* determine & code the front design + play with https://htmx.org/
+* Others things : TBD
+* Read & enjoy 🤩
---
+++ home/models/feed.py
@@ -0,0 +1,40 @@
+from django.db import models
+from model_utils.models import TimeStampedModel
+
+from . import Tag
+
+
+# https://fr.wikipedia.org/wiki/RSS
+
+class Feed(TimeStampedModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    link = models.URLField()
+    last_build_date = models.DateTimeField()
+    language = models.CharField(max_length=100)
+
+    tags = models.ManyToManyField(Tag, related_name="feed_tags", related_query_name="feed_tags")
+
+    def __str__(self) -> str:
+        return f"{self.title}: {self.url}"
+
+
+class FeedContent(TimeStampedModel):
+    guid = models.CharField(max_length=50)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    pub_date = models.DateTimeField()
+
+    CONTENT_TYPE = [
+        ('podcast', 'Podcast'),
+        ('article', 'Article'),
+    ]
+
+    content_type = models.CharField(max_length=25, choices=CONTENT_TYPE, default="article")
+
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+
+    tags = models.ManyToManyField(Tag, related_name="content_tags", related_query_name="content_tags")
+
+    def __str__(self) -> str:
+        return f"{self.guid}: {self.title}"
